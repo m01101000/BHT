a8a1c3ec84d507a1afc4333e176c751e1544e921
m01101000 <96919599+m01101000@users.noreply.github.com>
2023-05-08 18:52:58 +0200

    Update 2023_4._Python_4_Data_Science_Excercise_task1.py

--- Data Science (BHT MIM 20 S23)/4. Python 4 Data Science Excercise (15 points)/2023_4._Python_4_Data_Science_Excercise_task1.py
+++ Data Science (BHT MIM 20 S23)/4. Python 4 Data Science Excercise (15 points)/2023_4._Python_4_Data_Science_Excercise_task1.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#given: list = np.array([103, 1502, 6230, 3, 1682, 5241, 4532])
+#given: score = np.array([103, 1502, 6230, 3, 1682, 5241, 4532])
 sorted_indices_list = np.argsort(np.array([103, 1502, 6230, 3, 1682, 5241, 4532]))
 
 print("sorted_indices_list:", sorted_indices_list)
